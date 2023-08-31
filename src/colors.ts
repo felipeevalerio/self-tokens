6cccca37fcc8accd69895284759088b530dd711f
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2023-08-31 14:49:39 -0300

    feat: finalizando projeto tokens

---
+++ src/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    yellow500: '#FFCB74',
+    white: '#F6F6F6',
+    black500: '#111111',
+    black300: '#2f2f2f',
+    yellow300: '#FFEC99',
+    red500: '#F75A68',
+    gray500: '#A6A5A5'
+} as const
